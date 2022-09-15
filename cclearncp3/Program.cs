a8758dea0d2b50972e2e218dc29b0ebb8b4d38a4
jafar_hamza <jafar_hamza@G-02-11>
2022-09-15 09:58:20 -0400

    Add project files.

---
+++ cclearncp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cclearncp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int abc = 3;
+
+            if (abc > 3);
+            Console.WriteLine("abc is bigger than 3");
+             if (abc < 3);
+            Console.WriteLine("wassup");
+            
+
+        
+
+
+
+        }
+    }
+}
